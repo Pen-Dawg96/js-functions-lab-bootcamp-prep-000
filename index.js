50ffc7344de6cecd5afa6c65917198ec657b6a54
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-05 12:05:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ function happyHolidays(string){
 }
 
 function happyHolidaysTo(name){
-  console.log(`Happy holidays, ${name}!`)
+  return(`Happy holidays, ${name}!`)
 }
 
+
 function happyHolidayTo(holiday, name){
-  console.log(`Happy ${holiday}, ${name}!`)
+  return(`Happy ${holiday}, ${name}!`)
 }
 
 function holidayCountdown(holiday, days){
